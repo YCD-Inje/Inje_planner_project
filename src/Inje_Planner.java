0c9210a2deda499827d75a2942120a758655bd1c
Yeom1009 <yjg5641@naver.com>
2023-12-03 18:58:11 +0900

    패널 전환 작업 완료

--- src/Inje_Planner.java
+++ src/Inje_Planner.java
@@ -4,8 +4,11 @@ import javax.swing.*;
 import java.util.*;
 
 public class Inje_Planner extends JFrame{
+	
 	String[] option = {"메인 화면", "이번 주 계획", "다음 주 계획", "회원 정보 수정"};
 	JButton [] optionButton = new JButton [option.length];
+	
+	JPanel [] panel = new JPanel [4];
 	
 	public Inje_Planner() {
 		setTitle("InjePlanner");
@@ -14,26 +17,32 @@ public class Inje_Planner extends JFrame{
 		setVisible(true);
 		setLayout(new BorderLayout());
 		
+		for(int i = 0; i < 4; i++) {
+			panel[i] = new JPanel();
+			panel[i].add(new JLabel(Integer.toString(i)));
+			panel[i].setVisible(false);
+		}
+		panel[0].setVisible(true);
+		
+		
 		//메인 패널
-		JPanel main = new JPanel();
 		
-		
-		
-		//이번주 계획 패널
-		JPanel thisWeek = new JPanel();		
+		//이번주 계획 패널		
 				
-				
-		//다음주 계획 패널
-		JPanel nextWeek = new JPanel();	
-				
+		//다음주 계획 패널	
 				
 		//회원 정보 수정 패널
-		//최+당 만드는 중
+		//최+당 만드는 중	
 				
 				
-				
-				
-		//왼쪽 버튼 패널
+			
+		//center 패널
+		JPanel center = new JPanel();
+		for(int i = 0; i < 4; i++) {
+			center.add(panel[i]);
+		}
+		
+		//left 패널
 		JPanel left = new JPanel();
 		left.setLayout(new GridLayout(5, 1));
 		ImageIcon Diary = new ImageIcon("images/Diary.jpg");
@@ -46,10 +55,48 @@ public class Inje_Planner extends JFrame{
 		for (int i = 0; i < 4; i++) {
 			optionButton[i] = new JButton(option[i]);
 			left.add(optionButton[i]);
-		}     
+		} 
+		//왼쪽 버튼 누를때 마다 해당 패널이 나오고 기존 패널은 보이지 않게 설정
+		optionButton[0].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JButton btn0 = (JButton)e.getSource();
+				panel[0].setVisible(true);
+				panel[1].setVisible(false);
+				panel[2].setVisible(false);
+				panel[3].setVisible(false);
+			}
+		});
+		optionButton[1].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JButton btn1 = (JButton)e.getSource();
+				panel[0].setVisible(false);
+				panel[1].setVisible(true);
+				panel[2].setVisible(false);
+				panel[3].setVisible(false);
+			}
+		});
+		optionButton[2].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JButton btn2 = (JButton)e.getSource();
+				panel[0].setVisible(false);
+				panel[1].setVisible(false);
+				panel[2].setVisible(true);
+				panel[3].setVisible(false);
+			}
+		});
+		optionButton[3].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JButton btn3 = (JButton)e.getSource();
+				panel[0].setVisible(false);
+				panel[1].setVisible(false);
+				panel[2].setVisible(false);
+				panel[3].setVisible(true);
+			}
+		});
+		
 		        
 		        
-		//add(main, BorderLayout.CENTER);
+		add(center, BorderLayout.CENTER);
 		add(left, BorderLayout.WEST);
 	    }
 			
