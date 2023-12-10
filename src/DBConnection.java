3c019fe24bd0eb50a46dc4ecd57bc1f362b1caac
Good-jjun <65655340+Good-jjun@users.noreply.github.com>
2023-12-11 06:59:07 +0900

    Update DBConnection.java
    찐찐찐막

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,9 +1,14 @@
+package controller;
+
 import java.sql.*;
+import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import model.aa;
+import model.Planner;
 
 public class DBConnection extends JFrame{
 	private static int cnt =0;
@@ -12,11 +17,13 @@ public class DBConnection extends JFrame{
 	private ResultSet rs;
 	private String url = "jdbc:mysql://localhost:3306/inje?severTimezon=UTC";
 	private String user = "root";
-	private String password = "5641";
+	private String password = "chlwnsgur1!";
 	private static boolean isLoginFormVisible = false;
 	private static boolean isSignupFormVisible = false;
 	private PreparedStatement pstmt;
+	private String loggedInUserName;
 	private String loggedInUserId;
+	
 	/**DB 연결*/
 	
 	public String getLoggedInUserId() {
@@ -27,6 +34,8 @@ public class DBConnection extends JFrame{
 	public void setLoggedInUserId(String userId) {
         this.loggedInUserId = userId;
     }
+
+
 	
 	public String getPassword() {
 		try {
@@ -88,7 +97,7 @@ public class DBConnection extends JFrame{
 		public boolean addUser(String username, String password, String name, String department, String email, String phone)
 		{
 			 String sql = "INSERT INTO user (ID, pass, name, department, email, phone) VALUES (?, ?, ?, ?, ?, ?)";
-			 
+			 String sql1 = "INSERT INTO plan (user_id) VALUES (?)";
 			 try {
 				 PreparedStatement pstmt = con.prepareStatement(sql);
 			        pstmt.setString(1, username);
@@ -99,13 +108,22 @@ public class DBConnection extends JFrame{
 			        pstmt.setString(6, phone);
 				 
 			        int rowsAffected = pstmt.executeUpdate();
-			        return rowsAffected > 0;
-			 }catch (Exception e) {
-				 e.printStackTrace();
-					return false;
-		}
-	}
+			        // 첫 번째 쿼리의 결과가 성공일 때만 두 번째 쿼리 실행하기 
+			        if (rowsAffected > 0) {
+			            PreparedStatement pstmt2 = con.prepareStatement(sql1);
+			            pstmt2.setString(1, username);  
 
+			            int rowsAffected2 = pstmt2.executeUpdate();
+
+			            return rowsAffected2 > 0;
+			        } else {
+			            return false;
+			        }
+			    } catch (Exception e) {
+			        e.printStackTrace();
+			        return false;
+			    }
+			}
 		public boolean updateMember(String username, String password, String name, String department, String email, String phone)
 		{
 			 String sql = "UPDATE user SET pass = ?, name = ?, department = ?, email = ?, phone = ? WHERE ID = ?";
@@ -132,30 +150,31 @@ public class Login extends JFrame{
 }
 	
 	public static void main(String[] args) {
-		
+
 		JFrame frame = new JFrame("로그인 화면");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setSize(1200, 1000);
 	    frame.setLocationRelativeTo(null);
 	    frame.setLayout(new GridBagLayout());
        
+	    
 	    GridBagConstraints constraints = new GridBagConstraints();
 	    constraints.insets = new Insets(10, 10, 10, 10);
 
-	    ImageIcon imageIcon = new ImageIcon("images/Diary.png");
+	    ImageIcon imageIcon = new ImageIcon("image/Diary.png");
 	    JLabel imageLabel = new JLabel(imageIcon);
 
 	    JPanel buttonPanel = new JPanel();
 	    buttonPanel.setLayout(new FlowLayout());
 
 	    JButton loginButton = new JButton("");
-	    ImageIcon loginIcon = new ImageIcon("images/Login.png");
+	    ImageIcon loginIcon = new ImageIcon("image/Login.png");
 	    loginButton.setIcon(loginIcon);
 	    loginButton.setContentAreaFilled(false);
 	    loginButton.setBorder(BorderFactory.createEmptyBorder());
         
 	    JButton signupButton = new JButton("");
-	    ImageIcon signIcon = new ImageIcon("images/Sign up.png");
+	    ImageIcon signIcon = new ImageIcon("image/Sign up.png");
 	    signupButton.setIcon(signIcon);
 	    signupButton.setContentAreaFilled(false);
 	    signupButton.setBorder(BorderFactory.createEmptyBorder());
@@ -182,27 +201,35 @@ public class Login extends JFrame{
         gbc.gridx = 1;
         loginPanel.add(idField, gbc);
 
+
 	    JLabel pwLabel = new JLabel("PW:");
 	    JPasswordField pwField = new JPasswordField(15);
 	 
         gbc.gridx = 0;
         gbc.gridy = 1;
         loginPanel.add(pwLabel, gbc);
+
         
         gbc.gridx = 1;
         loginPanel.add(pwField, gbc);
 	    
 	    JButton loginConfirmButton = new JButton("");
-	    ImageIcon login1Icon = new ImageIcon("images/Login1.png");
+	    ImageIcon login1Icon = new ImageIcon("image/Login1.png");
 	    loginConfirmButton.setIcon(login1Icon);
 	    loginConfirmButton.setContentAreaFilled(false);
 	    loginConfirmButton.setBorder(BorderFactory.createEmptyBorder());
+	    
+	    
+	 
 	    
 	    gbc.gridx = 1;
         gbc.gridy = 2;
         gbc.anchor = GridBagConstraints.SOUTH; // Align to the right
         loginPanel.add(loginConfirmButton, gbc);
 
+	    
+	    
+	    
 	    
 	    JPanel signupPanel = new JPanel();
 	    signupPanel.setLayout(new GridBagLayout());
@@ -211,6 +238,8 @@ public class Login extends JFrame{
 	    GridBagConstraints gbcs = new GridBagConstraints();
         gbcs.insets.set(5 ,5 , 5 , 5); // Padding
         
+	   
+	    
 	    
 	    JLabel signupIdLabel = new JLabel("ID:");
 	    JTextField signupIdField = new JTextField(15);
@@ -234,12 +263,26 @@ public class Login extends JFrame{
 
         gbcs.gridx = 1;
         signupPanel.add(signupPwField, gbcs);
+        
+        JLabel confirmPasswordLabel = new JLabel("비밀번호 확인:");
+	    JPasswordField confirmPasswordField = new JPasswordField(15);
+	    
+        gbcs.gridx = 0;
+	    gbcs.gridy = 2;
+	    gbcs.anchor = GridBagConstraints.WEST;
+        signupPanel.add(confirmPasswordLabel, gbcs);
 
+        gbcs.gridx = 1;
+        signupPanel.add(confirmPasswordField, gbcs);
+        
+        
+        
+       
 	    JLabel nameLabel = new JLabel("이름:");
 	    JTextField nameField = new JTextField(15);
 	    
 	    gbcs.gridx = 0;
-	    gbcs.gridy = 2;
+	    gbcs.gridy = 3;
         signupPanel.add(nameLabel, gbcs);
 
         gbcs.gridx = 1;
@@ -251,7 +294,7 @@ public class Login extends JFrame{
 	    JTextField numberNoField = new JTextField(15);
 	    
 	    gbcs.gridx = 0;
-	    gbcs.gridy = 3;
+	    gbcs.gridy = 4;
         signupPanel.add(numberNoLabel, gbcs);
 
         gbcs.gridx = 1;
@@ -262,7 +305,7 @@ public class Login extends JFrame{
 	    JTextField departmentField = new JTextField(15);
 	    
 	    gbcs.gridx = 0;
-	    gbcs.gridy = 4;
+	    gbcs.gridy = 5;
         signupPanel.add(department, gbcs);
 
         gbcs.gridx = 1;
@@ -273,7 +316,7 @@ public class Login extends JFrame{
 	    JTextField emailField = new JTextField(15);
 	    
 	    gbcs.gridx = 0;
-	    gbcs.gridy = 5;
+	    gbcs.gridy = 6;
         signupPanel.add(email, gbcs);
 
         gbcs.gridx = 1;
@@ -281,21 +324,10 @@ public class Login extends JFrame{
 
 
 
-	    JLabel confirmPasswordLabel = new JLabel("비밀번호 확인:");
-	    JPasswordField confirmPasswordField = new JPasswordField(15);
-	    
-	    
-	    gbcs.gridx = 0;
-	    gbcs.gridy = 6;
-	    gbcs.anchor = GridBagConstraints.WEST;
-        signupPanel.add(confirmPasswordLabel, gbcs);
-
-        gbcs.gridx = 1;
-        signupPanel.add(confirmPasswordField, gbcs);
 	    
 	    JPanel signupConfirmPanel = new JPanel();
 	    JButton signupConfirmButton = new JButton("");
-	    ImageIcon Signup1Icon = new ImageIcon("images/Sign up1.png");
+	    ImageIcon Signup1Icon = new ImageIcon("image/Sign up1.png");
 	    signupConfirmButton.setIcon(Signup1Icon);
 	    signupConfirmButton.setContentAreaFilled(false);
 	    signupConfirmButton.setBorder(BorderFactory.createEmptyBorder());
@@ -353,7 +385,7 @@ public class Login extends JFrame{
 	            	dbConnection.setLoggedInUserId(userId);
 	            	frame.dispose();
 	                //TEST_home.main(new String[0]);
-	            	Save_Planner testHome = new Save_Planner(dbConnection);
+	            	Planner testHome = new Planner(dbConnection);
                     //testHome.setVisible(true);
 	              
 	            } else {
@@ -374,6 +406,8 @@ public class Login extends JFrame{
 	    	        String department = departmentField.getText();
 	    	        String email = emailField.getText();
 	    	        String phone = numberNoField.getText();
+	    	        String password1 = new String(confirmPasswordField.getPassword());
+	    	        
 	    	        // 데이터베이스에 연결
 	    	        DBConnection dbConnection = new DBConnection();
 	    	        dbConnection.connect();
@@ -384,7 +418,12 @@ public class Login extends JFrame{
 	    	        if (isSuccess) {
 	    	            // 회원가입 성공 메시지
 	    	            JOptionPane.showMessageDialog(frame, "회원가입 성공!");
-	    	        } else {
+	    	        } 
+	    	        else if(!password.equals(password1))
+	    	        {
+	    	        	 JOptionPane.showMessageDialog(frame, "비밀번호가 다릅니다. 다시 시도하세요.");
+	    	        } 
+	    	        else {
 	    	            // 회원가입 실패 메시지
 	    	            JOptionPane.showMessageDialog(frame, "회원가입 실패. 다시 시도하세요.");
 	    	        }
